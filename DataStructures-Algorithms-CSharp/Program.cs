188524a33f3221a637830f05995ef730b15f7e9a
abdulrahmansallam <abdulrahman.sallam.official@gmail.com>
2025-12-21 23:42:09 +0200

    Add project files.

---
+++ DataStructures-Algorithms-CSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures_Algorithms_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
