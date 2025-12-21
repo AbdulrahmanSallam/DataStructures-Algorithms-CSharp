1b3596499cc42825de7c71919536ce7f10780aa8
abdulrahmansallam <abdulrahman.sallam.official@gmail.com>
2025-12-21 23:47:50 +0200

    addStackArray

---
+++ DataStructures-Algorithms-CSharp/DataStructures/Stack/Array/StackArray.cs
@@ -0,0 +1,103 @@
+﻿namespace DataStructures_Algorithms_CSharp.DataStructures.Stack.Array
+{
+    internal class StackArray
+    {
+
+
+        private int TopIndex;
+        private int[] Entries;
+
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public StackArray(int capacity)
+        {
+
+            Entries = new int[capacity];
+            Capacity = capacity;
+            Count = 0;
+            TopIndex = -1;
+        }
+
+        public void Push(int item)
+        {
+            ResizeOrNot(Count);
+
+            Entries[Count] = item;
+
+            AddOrDeleteEntry(true);
+
+        }
+
+        public int? Pop()
+        {
+            if (TopIndex == -1) return null;
+
+            int deletedValue = Entries[TopIndex];
+
+            Entries[TopIndex] = 0;
+
+            AddOrDeleteEntry(false);
+
+            return deletedValue;
+        }
+
+        public void Print()
+        {
+            Console.Write("Items: ");
+            for (int i = TopIndex; i >= 0; i--)
+            {
+                Console.Write($"{Entries[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public int? Peek()
+        {
+            if (TopIndex == -1) return null;
+            return Entries[TopIndex];
+        }
+
+        public bool IsEmpty() => Count == 0;
+
+
+        // when the array is full make a new array with a douple of its capacity
+        private void ResizeOrNot(int count)
+        {
+            if (count >= Capacity)
+            {
+                Capacity = Capacity * 2;
+                int[] newEntries = new int[Capacity];
+
+                Entries.CopyTo(newEntries, 0);
+
+                Entries = null!;
+                Entries = newEntries;
+            }
+        }
+
+
+        private void AddOrDeleteEntry(bool add = true)
+        {
+            if (add)
+            {
+                TopIndex++;
+                Count++;
+                return;
+            }
+
+            TopIndex--;
+            Count--;
+        }
+
+
+        public override string ToString()
+        {
+            Print();
+            return $"Count: {Count}\n" +
+                   $"Capacity: {Capacity}\n" +
+                   $"Peek: {Peek()}\n" +
+                   $"IsEmpty: {IsEmpty()}";
+        }
+    }
+}
--- DataStructures-Algorithms-CSharp/Program.cs
+++ DataStructures-Algorithms-CSharp/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DataStructures_Algorithms_CSharp
+﻿using DataStructures_Algorithms_CSharp.DataStructures.Stack.Array;
+
+namespace DataStructures_Algorithms_CSharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region StackArrayBased
+            //StackArray arr = new StackArray(4);
+
+            //Console.WriteLine($"IsEmpty {arr.IsEmpty()}");
+
+            //Console.WriteLine("push 1:5");
+
+            //for (int i = 1; i <= 5; i++)
+            //    arr.Push(i);
+
+            //Console.WriteLine(arr.ToString());
+
+            //Console.WriteLine("push 6");
+            //arr.Push(6);
+
+            //Console.WriteLine(arr.ToString());
+
+            //for (int i = 1; i <= 7; i++)
+            //    Console.WriteLine($"Pop {arr.Pop()}");
+
+            //Console.WriteLine(arr.ToString());
+
+            #endregion
         }
     }
 }
